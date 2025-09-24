77e47c05a13b8fb68376778742b2a5a97c7d5688
Carlos Teran Sanchez <catesa@alumnatflorida.es>
2025-09-24 20:23:50 +0200

    2

--- DI/EnunciatsExercicisJS2.js
+++ DI/EnunciatsExercicisJS2.js
@@ -6,7 +6,7 @@ console.log(suma);
 
 //A partir d'un array d'strings i numeros, mostra un string que els continga tots
 let array2 = ['a', 1, 'b', 2, 'c', 3];
-const conca = array2.reduce((acc, vari) => acc +  vari, "");
+const conca = array2.reduce((acc, vari) => acc + vari, "");
 console.log(conca);
 // a1b2c3
 
@@ -14,25 +14,25 @@ console.log(conca);
 // És a dir per a l'array ['dilluns', 'dimarts', 'dimecres'] 
 // torne un objecte {dilluns:0, dimarts:1, dimecres:2}
 let dies = ['dilluns', 'dimarts', 'dimecres'];
-const ob = dies.reduce((ac, undia, pos) => { 
+const ob = dies.reduce((ac, undia, pos) => {
     ac[undia] = pos;
     return ac;
-},{ });
+}, {});
 console.log(ob);
 
 //A partir d'un array amb objectes Musics (nom i instrument que toca)
 //Que torne un array amb els noms de tots els components
 // [ 'Joan', 'Pep', 'Vicent Lloret', 'Ariadna', 'Guillem' ]
 let musics = [
-    {'nom': 'Joan', 'instrument': 'vocal'},
-    {'nom': 'Pep', 'instrument': 'guitarra'},
-    {'nom': 'Vicent Lloret', 'instrument': 'guitarra'},
-    {'nom': 'Ariadna', 'instrument': 'bateria'},
-    {'nom': 'Guillem', 'instrument': 'baix'}
+    { 'nom': 'Joan', 'instrument': 'vocal' },
+    { 'nom': 'Pep', 'instrument': 'guitarra' },
+    { 'nom': 'Vicent Lloret', 'instrument': 'guitarra' },
+    { 'nom': 'Ariadna', 'instrument': 'bateria' },
+    { 'nom': 'Guillem', 'instrument': 'baix' }
 ]
 let arrayMusicos = [];
 const nombres = musics.forEach(ele => arrayMusicos.push(ele.nom));
-console.log(arrayMusicos); 
+console.log(arrayMusicos);
 
 //Ara feu-ho utilitzant map()
 const nombres2 = musics.map(unMusico => unMusico.nom);
@@ -71,8 +71,8 @@ const users = [
         password: "Dean_hashed_password"
     }]
 
-    //Que torne un array d'objectes amb l'email, el tel i la web
-[
+//Que torne un array d'objectes amb l'email, el tel i la web
+/*[
     {
         email: 'Daphne43@yahoo.com',
         website: 'carmela.net',
@@ -83,9 +83,17 @@ const users = [
         website: 'dd.net',
         phone: '1-123-543-1857 123212'
     }
-]
-
-
+]*/
+// Forma uno
+/*const retorno = users.map((email, website, phone) => ({ email, website, phone }));
+console.log(retorno);*/
+// Forma 2 
+const retorn = users.map((elem) => ({
+    email: elem.email,
+    tel: elem.phone,
+    web: elem.website
+}));
+console.log(retorn);
 //-----------------------------------------------------
 //A partir d'un arrar de comandes com el proporcionat
 let comandes = [
@@ -134,7 +142,7 @@ let comandes = [
     }
 ]
 
-// tornar un array anomenat llistat de comandes, on cada element siga un array d'objectes on 
+// tornar un array anomenat llistat de comandes, on cada element siga un array d'objectes on
 // aparega només el client i el producte de la seua comanda.
 // Exemple:
 
@@ -153,6 +161,19 @@ let comandes = [
 //         { client: 'Tomas Marin', productes: 'Platans' }
 //     ]
 // ]
+const listado = comandes.map((elem) => ({
 
+    client: elem.client,
+    productes: elem.productes.map((ele) => ele.nom)
+}));
+console.log(listado);
 
+// Forma 2
+
+const listado2 = comandes.map((comanda) => {
+    comandes.productes.map((producte) => {
+        return { client: comanda.nom, productes: producte.nom }
+    })
+})
+console.log(listado2);
 
